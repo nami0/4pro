bfea65ffe1c397ecd3a0a0ae3d067fb3b66dccbe
nami0 <65006850+nami0@users.noreply.github.com>
2021-09-09 13:42:49 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var database = firebase.database();
 let room = "speech_room";
+const number = document.getElementById("number");
 const user = document.getElementById("user");
 const name = document.getElementById("name");
 const send = document.getElementById("send");
 const reset = document.getElementById("reset");
-const output1 = document.getElementById("output1");
-const output2 = document.getElementById("output2");
-const output3 = document.getElementById("output3");
+const output = document.getElementById("output");
 
 //音声認識
 const startBtn = document.querySelector('#start-btn');
@@ -23,13 +22,12 @@ recognition.interimResults = true;
 recognition.continuous = true;
 
 let finalTranscript = ''; // 確定した(黒の)認識結果
+
+//HTML表示
+let n = number.value;
 let str = [];
-let dataAll = [
-    {name: ""},{volume: 0},{script: ""},
-    {name: ""},{volume: 0},{script: ""},
-    {name: ""},{volume: 0},{script: ""}
-];
 let sumSpeechVolume = 0;
+let dataAll = [];
 
 recognition.onresult = (event) => {
     let interimTranscript = ''; // 暫定(灰色)の認識結果
@@ -41,11 +39,9 @@ recognition.onresult = (event) => {
 	    interimTranscript = transcript;
 	}
     }
-    resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
-    resultDiv2.innerHTML = finalTranscript.length; // 文字数
-    
     setSpeechVolume(finalTranscript);
     getLatestData();
+    writeSpeechVolume(finalTranscript,interimTranscript);
 }
 
 
@@ -59,7 +55,7 @@ stopBtn.onclick = () => {
 
 //送信処理
 send.addEventListener('click', function() {
-    database.ref(room+'/'+user.value).set({
+    database.ref(room+'/'+"user"+user.value).set({
 	name: name.value,
 	speechVolume: finalTranscript.length,
 	script: finalTranscript,
@@ -67,49 +63,44 @@ send.addEventListener('click', function() {
     getLatestData();
 });
 reset.addEventListener('click', function() {
-    database.ref(room+'/'+user.value).set({
+    database.ref(room+'/'+"user"+user.value).set({
 	name: "",
 	speechVolume: 0,
 	script: "",
     });
+    finalTranscript = '';
+    writeSpeechVolume(finalTranscript,'');
     getLatestData();
 });
 //データを更新
 function setSpeechVolume(finalTranscript){
-    database.ref(room+'/'+user.value).set({
+    database.ref(room+'/'+"user"+user.value).set({
 	name: name.value,
 	speechVolume: finalTranscript.length,
 	script: finalTranscript,
     });
 }
+//音声認識部分のHTML表示
+function writeSpeechVolume(finalTranscript,interimTranscript){
+    resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+    resultDiv2.innerHTML = finalTranscript.length; // 文字数
+}
 
 //chart表示
 let data = [];
+let rgb=[];
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     type: 'line',
     data: {
-	datasets: [{
-	    label:"user1",
-	    borderColor: "rgba(255,0,0,1)",
-	    backgroundColor:"rgba(255,0,0,0)",
-	    data: [],
-	},{
-	    label:"user2",
-	    borderColor: "rgba(0,255,0,1)",
-	    backgroundColor:"rgba(0,255,0,0)",
-	    data: [],
-	},{
-	    label:"user3",
-	    borderColor: "rgba(0,0,255,1)",
-	    backgroundColor:"rgba(0,0,255,0)",
-	    data: [],
-	}]
+	datasets: []
     },
     options: {
 	scales: {
-	    x: {
+	    xAxes: {
 		type: 'realtime',
+		display: false,
+		
 		realtime: {
 		    duration: 20000,  // 過去20000ミリ秒のデータを表示
 		    refresh: 1000,    // onRefresh コールバックを1000ミリ秒毎に呼び出し
@@ -117,59 +108,153 @@ var chart = new Chart(ctx, {
 		    pause: false,     // チャートは一時停止していない
 		    ttl: undefined,   // データはチャートから消えると自動的に削除
 		    frameRate: 30,    // データポイントを毎秒30回描画
-
 		    
 		    onRefresh: chart => {
-			for(let i=0; i<3; i++) {
-			    getLatestData();
-			    chart.data.datasets[i].data.push(data[i]);
+			getLatestData();
+			for(let i=0; i<n; i++) {
+			    if(chart.data.datasets[i] == undefined){
+				if(dataAll[i] != undefined){
+				    writeDatasets(i);
+				}
+			    } else {
+				chart.data.datasets[i].data.push(data[i]);
+				chart.data.datasets[i].label = setLabel(i);
+				updateYAxes();
+			    }
 			}
 		    }
 		}
+	    },
+	    yAxes: {
+		min: 0,
 	    }
 	}
     }
 });
 //データベースからデータを読み取る
 function getLatestData() {
+    n = number.value;
     const dbRef = firebase.database().ref(room);
-    for(let i=1; i<4; i++){
-	dbRef.child("user"+i).get().then((snapshot) => {
+    for(let i=0; i<n; i++){
+	dbRef.child("user"+(i+1)).get().then((snapshot) => {
 	    if (snapshot.exists()) {
 		const v = snapshot.val();
 		const k = snapshot.key;
-		data[i-1] = {
-		    x: Date.now(),
-		    y: v.speechVolume
-		}
-		dataAll[i-1] = {
+		dataAll[i] = {
 		    name: v.name,
 		    volume: v.speechVolume,
 		    script: v.script
 		}
 	    } else {
-		console.log("No data available");
+		dataAll[i] = {
+		    name: "",
+		    volume: 0,
+		    script: ""
+		}
 	    }
+	    //console.log("No data available");
 	}).catch((error) => {
 	    console.error(error);
 	});
+	if(dataAll[i] === undefined){
+	    data[i] = {
+		x: Date.now(),
+		y: 0
+	    };
+	}else{
+	    data[i] = {
+		x: Date.now(),
+		y: dataAll[i].volume
+	    };
+	}
     }
     writeHTML();
 }
-//HTML表示
-function writeHTML() {
-    for(let i=0; i<3; i++) sumSpeechVolume += dataAll[i].volume;
-    for(let i=0; i<3; i++){
-	str[i] = '<hr>';
-	str[i] += '<div class="name">名前：'+dataAll[i].name+'</div>';
-	str[i] += '<div class="text">発言量：'+dataAll[i].volume+'</div>';
-	str[i] += '<div class="script">発言内容：'+dataAll[i].script+'</div>';
-	str[i] += "発言の割合: " + ( dataAll[i].volume / sumSpeechVolume ) * 100 + "%";
-	str[i] += '<hr>';
+function writeDatasets(i) {
+    if(rgb[i] == null){
+	rgb[i] = {
+	    r: randomColor(),
+	    g: randomColor(),
+	    b: randomColor(),
+	}
     }
-    output1.innerHTML = str[0];
-    output2.innerHTML = str[1];
-    output3.innerHTML = str[2];
-    sumSpeechVolume = 0;
+    chart.data.datasets[i] = ({
+	label: setLabel(i),
+	borderColor: "rgba("+rgb[i].r+","+rgb[i].g+","+rgb[i].b+",1)",
+	backgroundColor:"rgba("+rgb[i].r+","+rgb[i].g+","+rgb[i].b+",0.4)",
+	data: [],
+    });
+    chart.update();
+}
+function setLabel(i){
+    let label;
+    if(dataAll[i].name != ""){
+	label = dataAll[i].name;
+    } else {
+	label = "No."+(i+1);
+    }
+    return label;
+}
+function randomColor() {
+    let rgb = Math.floor(Math.random() * (255 + 1));
+    return rgb;
+}
+function updateYAxes(){
+    let max = 0;
+    let min = data[0].y;
+    for(let i=0; i<n; i++){
+	if(data[i].y > max){
+	    max = data[i].y;
+	}
+	if(data[i].y < min){
+	    min = data[i].y;
+	}
+    }
+    if(min >= 10){
+	chart.options.scales.yAxes.min = min - 10;
+    } else {
+	chart.options.scales.yAxes.min = 0;
+    }
+    chart.options.scales.yAxes.max = max + 10;
 }
 
+//HTML表示
+function writeHTML() {
+    for(let i=0; i<n; i++){
+	if(dataAll[i] === undefined){
+	} else {
+	    sumSpeechVolume += dataAll[i].volume;
+	}
+	
+    }
+    for(let i=0; i<n; i++){
+	if(dataAll[i] === undefined){
+	    str[i] = "loading..."
+	} else {
+	    writeStr(i);
+	}
+    }
+    
+    output.innerHTML = outputStr(str);
+    sumSpeechVolume = 0;
+}
+function writeStr(i) {
+    if(dataAll[i].name=="" && dataAll[i].volume==0 && dataAll[i].script==""){
+	str[i] = "No data available"
+    } else {
+	str[i] = '<div class="name">No.'+(i+1)+'　名前：'+dataAll[i].name+'</div>';
+	str[i] += '<div class="volume">発言量：'+dataAll[i].volume+' ';
+	if(sumSpeechVolume != 0){
+	    str[i] += "("+Math.floor((dataAll[i].volume / sumSpeechVolume) * 100)+"%)";
+	}
+	str[i] += '</div><div class="script">発言内容：'+dataAll[i].script+'</div>';
+    }
+}
+function outputStr(str) {
+    let s = '<hr>';
+    for(let i=0; i<n; i++){
+	s += str[i];
+	s += '<hr>';
+    }
+    return s;
+}
